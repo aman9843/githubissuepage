1a8d818a57a270e8388bf95295390f63b0f92617
aman9843 <amanprasad9843@gmail.com>
2022-02-16 15:18:07 +0530

    commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import {ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
